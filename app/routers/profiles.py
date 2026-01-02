071c2be6a51de62d84152704b28de1af6c067ca2
Jhon Rosell Talisic <jhonroselltalisic994@gmail.com>
2026-01-02 20:30:20 +0800

    Add template for Profiles endpoint

--- app/routers/profiles.py
+++ app/routers/profiles.py
@@ -1,31 +1,17 @@
+
+from uuid import UUID
 from fastapi import APIRouter
-from app.schemas.profiles import ProfilesBase, ProfilesCreate
+from app.schemas.profiles import ProfilesBase, ProfilesCreate, ProfilesRead
 
 router = APIRouter(prefix="/api/profiles", tags=["Profiles"])
 
-profile_list = [
-    ProfilesCreate(username="user1", bio="Bio of user1", profile_pic_path="/images/user1.jpg"),
-    ProfilesCreate(username="user2", bio="Bio of user2", profile_pic_path="/images/user2.jpg"),
-    ProfilesCreate(username="user3", bio="Bio of user3", profile_pic_path="/images/user3.jpg"),
-    ]
+# Get user profile
+@router.get("/me", response_model=ProfilesRead)      
+async def get_profile(user_id: UUID):
+    pass
 
-# Path var
-@router.get("/{profile_id}", response_model=ProfilesBase)
-def GetProfile(profile_id: str):
-    return {
-        "username": profile_id,
-        "bio": "This is a sample bio.",
-        "profile_pic_path": "/images/sample.jpg"
-    }
-
-@router.get("",response_model=dict[str, list[ProfilesBase]])    
-def GetAllProfiles():
-    return {
-        "profile_list": profile_list
-    }
-
-# Query param
-
-
-# Req body
+# Update user profile (think about using id or email based from the jwt)
+@router.patch("/{user_id}", response_model=ProfilesBase)
+async def update_profile(user_id: UUID, new_profile: ProfilesCreate):
+    pass
 
